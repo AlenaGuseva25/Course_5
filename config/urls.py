98806f8d573ffbd69c8c931c4f08510e5f788d5f
Alena <alittlekindness94@gmail.com>
2025-10-16 18:05:19 +0500

    итоговое

--- config/urls.py
+++ config/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('users/', include('users.urls', namespace='users')),
     path('', include('habits.urls', namespace='habits')),
 
-# Документация
+    # Документация
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
